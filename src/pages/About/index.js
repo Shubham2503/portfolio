ddac315e0697f11b47c09e425bb89914c9cafefb
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-12-25 23:23:10 +0530

    added About Page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
-
+import styles from './index.module.css'
+import image from './../../assets/image.jpg'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const About = () => {
 
     return (
-        <div>
+        <div className={styles.container} >
+            <Container fluid={true} >
+                <Row className={styles.title}>
+                    <h1>About me...!</h1>
+                </Row>
+                <Row >
+                    <Col md={4} style={{ textAlign: "center", fontFamily: "serif" }} >
+                        <img className={styles.profile} src={image} />
+                        <h4 style={{ fontFamily: "serif" }}>Shubham Patel</h4>
+                        <h4>Ping me !!!</h4>
+                        <a>Email </a>
+                        <a>LinkedIn</a>
+                    </Col>
+                    <Col>
+                        <h4 style={{ marginBottom: "0px" }}>Hi, I'm Shubham Patel.</h4>
+                        <h4>
+                            I am currently pursuing my B.Tech in Computer engineering from Sardar Vallabhbhai National Institute of technology (SVNIT).
+                        </h4>
 
+                        <h2>Recent Accomplishments </h2>
+                        <ul>
+                            <li>
+                                Finalist at JP Morgan CFG Hackthon 2020
+                            </li>
+                        </ul>
+                        <h2>Education</h2>
+                        <ul>
+                            <li>SVNIT</li>
+                            <li>HSC</li>
+                            <li>SSC</li>
+                        </ul>
+                        <h2>Certificates</h2>
+                        <ul>
+                            <li>ML</li>
+                            <li>Algorithemic Toolbox</li>
+                        </ul>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
+
     );
 }
 
